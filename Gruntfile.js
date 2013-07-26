a0e34706b8419cac55b8dc2744e4f0467b5b7d58
pokornyr <robin.pokorny@lmc.eu>
2013-07-26 14:33:05 +0200

    Initial settings

---
+++ Gruntfile.js
@@ -0,0 +1,28 @@
+/*global module:false*/
+module.exports = function(grunt) {
+    grunt.loadNpmTasks('grunt-contrib-less');
+
+    // Project configuration.
+    grunt.initConfig({
+    // Metadata.
+    pkg: grunt.file.readJSON('package.json'),
+    banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
+        '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+        '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
+        '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
+        ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
+    // Task configuration.
+    less: {
+        dist: {
+            files: {
+                "css/emerald.css": "less/emerald.less"
+            }
+        }
+    }
+    });
+
+    // Default task.
+    grunt.registerTask('default', ['less']);
+
+};
+
--- README.md
+++ README.md
@@ -1,4 +1,19 @@
-emerald
+Emerald
 =======
 
 Emerald is a responsive grid system written in LESS
+
+How to use
+-----------
+It is advised to include the LESS files in your project and compile it with your build sript.
+
+If you need (or realy want) the CSS make sure you have node and npm.
+Than install dependencies:
+```
+$ npm install
+```
+The CSS is generated simply by running:
+```
+$ grunt
+```
+
