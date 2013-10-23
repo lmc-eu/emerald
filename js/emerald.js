12090606527225a51764ccb096cfc4278b2bed54
pokornyr <robin.pokorny@lmc.eu>
2013-10-23 15:35:54 +0200

    upgrade to v0.6.1

--- js/emerald.js
+++ js/emerald.js
@@ -19,54 +19,57 @@ var supportsVw = function() {
     return width === compStyle;
 }();
 
+/* 
+ * Define parameters and the emulating function
+ */
+var columnsCount = 8
+    columnWidth = 6,
+    gutterWidth = 6,
+    screenLap = 768,
+    $style = $('<style />' ).appendTo('head'),
+    emulateEmarald = function() {
+        var width = parseInt(window.innerWidth,10);
+
+        // Do not continue if not on mobile
+        if (width > screenLap) {
+            return;
+        }
+
+        var vw = width / 100,
+            style = '@media only screen and (max-width: ' +
+                screenLap + 'px) {';
+
+        // Set width for each column size
+        for (var i = 1; i <= columnsCount; i++) {
+            style += '.e-' + i + '--palm {width: ' +
+                ((columnWidth + gutterWidth) * i * vw) + 'px;}';
+
+            style += '.e-push-' + i + '--palm {margin-left: ' +
+                ((columnWidth + gutterWidth) * i * vw) + 'px;}';
+
+            style += '.e-pull-' + i + '--palm {margin-right: ' +
+                ((columnWidth + gutterWidth) * i * vw) + 'px;}';
+        }
+
+        style += '.grid {margin-right: ' +
+            (- (gutterWidth / 2) * vw) + 'px;margin-left: ' +
+            (- (gutterWidth / 2) * vw) + 'px;}';
+
+        style += '.grid__item {padding-right: ' +
+            ((gutterWidth / 2) * vw) + 'px;padding-left: ' +
+            ((gutterWidth / 2) * vw) + 'px;}';
+
+        style += '.wrapper {width: ' +
+            ((columnWidth + gutterWidth) * columnsCount * vw) +
+            'px;padding-right: ' + ((gutterWidth / 2) * vw) +
+            'px;padding-left: ' + ((gutterWidth / 2) * vw) + 'px;}';
+
+        style += '}';
+
+        $style.html(style);
+    };
+
 if (!supportsVw) {
-    var columnsCount = 8
-        columnWidth = 6,
-        gutterWidth = 6,
-        screenLap = 768,
-        $style = $('<style />' ).appendTo('head'),
-        emulateEmarald = function() {
-            var width = parseInt(window.innerWidth,10);
-
-            // Do not continue if not on mobile
-            if (width > screenLap) {
-                return;
-            }
-
-            var vw = width / 100,
-                style = '@media only screen and (max-width: ' +
-                    screenLap + 'px) {';
-
-            // Set width for each column size
-            for (var i = 1; i <= columnsCount; i++) {
-                style += '.e-' + i + '--palm {width: ' +
-                    ((columnWidth + gutterWidth) * i * vw) + 'px;}';
-
-                style += '.e-push-' + i + '--palm {margin-left: ' +
-                    ((columnWidth + gutterWidth) * i * vw) + 'px;}';
-
-                style += '.e-pull-' + i + '--palm {margin-right: ' +
-                    ((columnWidth + gutterWidth) * i * vw) + 'px;}';
-            }
-
-            style += '.grid {margin-right: ' +
-                (- (gutterWidth / 2) * vw) + 'px;margin-left: ' +
-                (- (gutterWidth / 2) * vw) + 'px;}';
-
-            style += '.grid__item {padding-right: ' +
-                ((gutterWidth / 2) * vw) + 'px;padding-left: ' +
-                ((gutterWidth / 2) * vw) + 'px;}';
-
-            style += '.wrapper {width: ' +
-                ((columnWidth + gutterWidth) * columnsCount * vw) +
-                'px;padding-right: ' + ((gutterWidth / 2) * vw) +
-                'px;padding-left: ' + ((gutterWidth / 2) * vw) + 'px;}';
-
-            style += '}';
-
-            $style.html(style);
-        };
-
     emulateEmarald();
     $(window).resize(emulateEmarald);
 }
