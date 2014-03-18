5bd558a79747395de2c212fca312bcf4527aafc0
Robin Pokorny <Robin.Pokorny@lmc.eu>
2014-03-18 10:34:51 +0100

    Update to the current version of the JS file

--- js/emerald.js
+++ js/emerald.js
@@ -2,74 +2,94 @@
  * A workaround mainly for Android Browser emulating the vw units
  */
 
-/*
- * Viewport units support test
- * Based on Modernizr: https://github.com/Modernizr/Modernizr/blob/master/feature-detects/css/vwunit.js
- */
-var supportsVw = function() {
-    var $elem = $('<div />').appendTo('body').css('width', '50vw');
+(function () {
+    'use strict';
+    /*jslint browser:true */
 
-    var width = (parseInt(window.innerWidth,10) / 2),
-        compStyle = parseInt((window.getComputedStyle ?
-                    getComputedStyle($elem[0], null) :
-                    $elem[0].currentStyle)['width'],10);
+    /*
+     * Legacy browsers (e.g. IE8) should be served the desktop version only
+     * and therefore do not need to emulate mobile grid.
+     */
+    if (!Element.prototype.addEventListener) {
+        return;
+    }
 
-    $elem.remove();
+    /*
+     * Viewport units support test
+     * Based on Modernizr: https://github.com/Modernizr/Modernizr/blob/master/feature-detects/css/vwunit.js
+     */
+    var supportsVw = (function () {
+        var elem, width, compStyle, compWidth;
+        elem = document.createElement("div");
+        elem.style.width = '50vw';
+        document.body.appendChild(elem);
 
-    return width === compStyle;
-}();
+        width = parseInt(window.innerWidth / 2, 10);
+        compStyle = window.getComputedStyle ?
+                    window.getComputedStyle(elem, null) :
+                    elem.currentStyle;
+        compWidth = parseInt(compStyle.width, 10);
 
-/* 
- * Define parameters and the emulating function
- */
-var columnsCount = 8
-    columnWidth = 6,
-    gutterWidth = 6,
-    screenLap = 768,
-    $style = $('<style />' ).appendTo('head'),
-    emulateEmarald = function() {
-        var width = parseInt(window.innerWidth,10);
+        document.body.removeChild(elem);
 
-        // Do not continue if not on mobile
-        if (width > screenLap) {
-            return;
-        }
+        return width === compWidth;
+    }()),
 
-        var vw = width / 100,
+        /* 
+         * Define parameters and the emulating function
+         */
+        columnsCount = 8,
+        columnWidth = 6,
+        gutterWidth = 6,
+        screenLap = 768,
+        headStyle = document.createElement("style"),
+        emulateEmerald = function () {
+            var width, vw, style, i;
+            width = parseInt(window.innerWidth, 10);
+
+            // Do not continue if not on mobile
+            if (width > screenLap) {
+                headStyle.innerHTML = "";
+                return;
+            }
+
+            vw = width / 100;
             style = '@media only screen and (max-width: ' +
                 screenLap + 'px) {';
 
-        // Set width for each column size
-        for (var i = 1; i <= columnsCount; i++) {
-            style += '.e-' + i + '--palm {width: ' +
-                ((columnWidth + gutterWidth) * i * vw) + 'px;}';
+            // Set width for each column size
+            for (i = 1; i <= columnsCount; i += 1) {
+                style += '.e-' + i + '--palm {width: ' +
+                    ((columnWidth + gutterWidth) * i * vw) + 'px;}';
 
-            style += '.e-push-' + i + '--palm {margin-left: ' +
-                ((columnWidth + gutterWidth) * i * vw) + 'px;}';
+                style += '.e-push-' + i + '--palm {margin-left: ' +
+                    ((columnWidth + gutterWidth) * i * vw) + 'px;}';
 
-            style += '.e-pull-' + i + '--palm {margin-right: ' +
-                ((columnWidth + gutterWidth) * i * vw) + 'px;}';
-        }
+                style += '.e-pull-' + i + '--palm {margin-right: ' +
+                    ((columnWidth + gutterWidth) * i * vw) + 'px;}';
+            }
 
-        style += '.grid {margin-right: ' +
-            (- (gutterWidth / 2) * vw) + 'px;margin-left: ' +
-            (- (gutterWidth / 2) * vw) + 'px;}';
+            style += '.grid {margin-right: ' +
+                (-(gutterWidth / 2) * vw) + 'px;margin-left: ' +
+                (-(gutterWidth / 2) * vw) + 'px;}';
 
-        style += '.grid__item {padding-right: ' +
-            ((gutterWidth / 2) * vw) + 'px;padding-left: ' +
-            ((gutterWidth / 2) * vw) + 'px;}';
+            style += '.grid__item {padding-right: ' +
+                ((gutterWidth / 2) * vw) + 'px;padding-left: ' +
+                ((gutterWidth / 2) * vw) + 'px;}';
 
-        style += '.wrapper {width: ' +
-            ((columnWidth + gutterWidth) * columnsCount * vw) +
-            'px;padding-right: ' + ((gutterWidth / 2) * vw) +
-            'px;padding-left: ' + ((gutterWidth / 2) * vw) + 'px;}';
+            style += '.wrapper {width: ' +
+                ((columnWidth + gutterWidth) * columnsCount * vw) +
+                'px;padding-right: ' + ((gutterWidth / 2) * vw) +
+                'px;padding-left: ' + ((gutterWidth / 2) * vw) + 'px;}';
 
-        style += '}';
+            style += '}';
 
-        $style.html(style);
-    };
+            headStyle.innerHTML = style;
+        };
 
-if (!supportsVw) {
-    emulateEmarald();
-    $(window).resize(emulateEmarald);
-}
+    if (!supportsVw) {
+        document.head.appendChild(headStyle);
+        emulateEmerald();
+        window.addEventListener('resize', emulateEmerald);
+    }
+}());
